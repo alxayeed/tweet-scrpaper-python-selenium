90abfee2b20ab1b3fb8e3b5fec689860e7c405e7
alxayeed <alxayeed@gmail.com>
2021-04-04 15:06:43 +0600

    tweet updated to use waiting_func

--- tweet scrapper/tweet_scrapper.py
+++ tweet scrapper/tweet_scrapper.py
@@ -2,20 +2,31 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+
 
 # Access to twitter
 url = 'https://twitter.com'
 driver = webdriver.Chrome()
 driver.get(url)
 
-try:
-    login_btn = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.LINK_TEXT, "Log in"))
-    )
 
-finally:
-    print(login_btn)
-    login_btn.click()
+def waiting_func(by_variable, attribute):
+    try:
+        WebDriverWait(driver, 20).until(
+            lambda x: x.find_element(by=by_variable,  value=attribute))
+    except (NoSuchElementException, TimeoutException):
+        print('{} {} not found'.format(by_variable, attribute))
+        exit()
 
 
-# login_btn = driver.find_element_by_xpath("//div[@class='css-1dbjc4n']")
+# try:
+#     login_btn = WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located((By.LINK_TEXT, "Log in"))
+#     )
+# finally:
+#     login_btn.click()
+
+waiting_func('link text', 'Log in')
+login_btn = driver.find_element_by_link_text('Log in')
+login_btn.click()
